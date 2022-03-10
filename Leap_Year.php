1298ffe42e344b3297e462ea44c34059d0f303ae
“ArafathvBaig” <arafathbaig1997@gmail.com>
2022-03-11 00:55:52 +0530

    Updated: Leap_Year.php program to the master

--- Leap_Year.php
+++ Leap_Year.php
@@ -2,8 +2,10 @@
 $year = readline('Enter a Year: ');
 $length = strlen((string) $year);
 if ($length == 4) {
-    if ($year % 100 == 0 && $year % 400 == 0) {
+    if ($year % 400 == 0) {
         echo $year . " is Leap Year.";
+    } elseif ($year % 100 == 0) {
+        echo $year . " is Not Leap Year.";
     } elseif ($year % 4 == 0) {
         echo $year . " is Leap Year.";
     } else {
